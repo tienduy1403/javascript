f1a08786420713119c5620e82c78747314a9051c
tienduy1403 <tienduy1403@gmail.com>
2024-01-29 22:15:33 +0700

    Bài 13: thêm while

--- while-for/bai13.js
+++ while-for/bai13.js
@@ -3,11 +3,23 @@ Dưới đây là một đoạn mã JavaScript để in ra bảng cửu chương
 */
 
 // Lặp vòng ngoài (vòng bảng) từ 1 đến 10
-for (let i = 1; i <= 10; i++) {
-    // Lặp vòng trong tăng giá trị từ 1 đến 10
-    for (let n = 1; n <= 10; n++) {
-        multiplication = i * n;
-        console.log(`${i} x ${n} = ${multiplication}`);
+// for (let i = 1; i <= 10; i++) {
+//     // Lặp vòng trong tăng giá trị từ 1 đến 10
+//     for (let n = 1; n <= 10; n++) {
+//         multiplication = i * n;
+//         console.log(`${i} x ${n} = ${multiplication}`);
+//     }
+//     console.log("\n");
+// }
+
+let n = 1;
+while (n <= 10) {
+    let i = 1;
+    while (i <= 10) {
+        let multiplication = n * i;
+        console.log(`${n} x ${i} = ${multiplication}`);
+        i++;
     }
     console.log("\n");
+    n++;
 }
