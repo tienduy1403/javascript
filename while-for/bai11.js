ee5c070f37b851551f25922889b77b1fb85f30fe
tienduy1403 <tienduy1403@gmail.com>
2024-01-29 20:40:46 +0700

    Bài 11: Sử dụng vòng lặp để in ra dãy số từ 1 đến 10 trên cùng 1 hàng.

---
+++ while-for/bai11.js
@@ -0,0 +1,12 @@
+/*
+Sử dụng vòng lặp để in ra dãy số từ 1 đến 10.
+*/
+
+// Dùng for
+let myString = "";
+for (let i = 1; i <= 10; i++) {
+    // In ra 1 dãy số trên cùng 1 hàng;
+    myString += `${i}, `;
+}
+
+console.log(myString);
